b186fc72c982a685aab1d59b7090efd3390dbd2e
Varun Chinthoju <varun.chinthoju@gmail.com>
2024-05-10 20:11:27 -0700

    my baseline

---
+++ pygame_Learning/copy_this_into_a_new_pygame_file_for_the_baseline.py
@@ -0,0 +1,22 @@
+import pygame
+from pygame.locals import *
+def main():
+    SCREENWIDTH = 800
+    SCREENHEIGHT = 600
+    COLOR = (255, 255, 255)
+    screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+    fps = 60
+    running = True
+    clock = pygame.time.Clock()
+    while running:
+        clock.tick(fps)
+        screen.fill(COLOR)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        pygame.display.update()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
