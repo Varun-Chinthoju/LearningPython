f30222c5a98c24e5999cd51e8503cb012d0606b2
Varun Chinthoju <varun.chinthoju@gmail.com>
2024-05-17 19:23:55 -0700

    when you dont wanna do hw:

---
+++ IQR.py
@@ -0,0 +1,28 @@
+def find_iqr_quartiles(numbers):
+  if not numbers:
+    raise ValueError("Input list cannot be empty")
+
+  sorted_numbers = sorted(numbers)
+
+  median_index = len(sorted_numbers) // 2
+  median = sorted_numbers[median_index]
+
+  if len(sorted_numbers) % 2 == 0:
+    Q1_index = (median_index - 1) // 2
+  else:
+    Q1_index = median_index // 2
+  Q1 = sorted_numbers[Q1_index]
+
+  if len(sorted_numbers) % 2 == 0:
+    Q3_index = median_index + (len(sorted_numbers) // 2) // 2
+  else:
+    Q3_index = median_index + (median_index // 2) + 1
+  Q3 = sorted_numbers[Q3_index]
+
+  IQR = Q3 - Q1
+
+  return {"IQR": IQR, "Q1": Q1, "Q3": Q3}
+
+numbers = [1, 2, 3, 3, 5, 7, 8, 9]
+results = find_iqr_quartiles(numbers)
+print(results)
