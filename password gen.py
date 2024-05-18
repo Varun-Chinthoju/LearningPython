2f851fc7e75224afa39a0ec9d38dd2940dc056be
Varun Chinthoju <varun.chinthoju@gmail.com>
2024-05-18 11:52:46 -0700

    added comments

--- password gen.py
+++ password gen.py
@@ -2,15 +2,15 @@ import random
 import string
 import os
 
-os.system("cls" if os.name == "nt" else "clear") 
+os.system("cls" if os.name == "nt" else "clear") #clears terminal before inputting password
 
 
 def generate_password(
     length=8, lowercase=True, uppercase=True, digits=True, punctuation=True
 ):
-
+#password string
     all_chars = ""
-
+#makes the password
     if lowercase:
         all_chars += string.ascii_lowercase
     if uppercase:
@@ -19,10 +19,10 @@ def generate_password(
         all_chars += string.digits
     if punctuation:
         all_chars += string.punctuation
-
+#joins the password
     password = "".join(random.sample(all_chars, length))
     return password
 
-
+#prints the password using parameters length, lowercase, uppercase, digits, and punctuation
 password = generate_password(12)
 print(password)
