b7df286de3cf5b7594936629207155277b68564f
Varun Chinthoju <varun.chinthoju@gmail.com>
2024-05-20 21:11:48 -0700

    uses a variable called pw count to determine if the user has already asked for a password and uses different words

--- password gen.py
+++ password gen.py
@@ -3,10 +3,9 @@ import string
 import os
 
 # first checks if
-os.system(
-    "cls" if os.name == "nt" else "clear"
-)  # clears terminal before inputting password
-
+ # clears terminal before inputting password
+os.system("cls" if os.name == "nt" else "clear") 
+pw_count = 0
 
 def generate_password(
     length=8, lowercase=True, uppercase=True, digits=True, punctuation=True
@@ -26,14 +25,20 @@ def generate_password(
     password = "".join(random.sample(all_chars, length))
     return password
 
-
+print("Welcome to the password generator!")
 # asks user if they want to generate a password until they deny
 while True:
-    response = input("Would you like to generate a password? (yes/no): ")
+    if pw_count == 0:
+        response = input("Would you like to generate a password? (yes/no): ")
+    else:
+        response = input("Would you like to generate a new password? (yes/no): ")
     if response.lower() == "yes":
+        os.system("cls" if os.name == "nt" else "clear")
+        pw_count+=1
         password = generate_password(12)
         print(password)
     elif response.lower() == "no":
+        print("Goodbye!")
         exit()
     else:
         print("Invalid response. Please enter 'yes' or 'no'.")
